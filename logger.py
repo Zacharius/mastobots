931008b24eb4c71fd399df3c0b597902addd3658
Zach Faddis <zach.faddis@gmail.com>
2018-08-06 11:10:39 -0500

    class to make a log of the rss feeds being tooted

---
+++ logger.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+
+import os
+import re
+import time
+import subprocess
+import datetime
+
+def main():
+    logDir = "/home/zacharius/Project/Mastobots/log/"
+    logfile = "out.txt"
+    logger = Logger(logDir, logfile)
+
+    text = "One Potato is not enough to make a stew"
+    title = "poetry"
+    link = "zfadd.is"
+    author = "@zacharius"
+
+    logger.log(text, title, author, link)
+    logger.log(text, title, author, link)
+
+    logger.archive()
+
+class Logger:
+
+    def __init__(self, logDir, logFile):
+        self.logfile = str(logDir +  logFile)
+
+        
+    def log(self, text, title, author, link):
+
+        with open(self.logfile, "a") as log:
+
+            log.write('<a href="' + link + '">' + title + '</a> ')
+            log.write('by ' + author + '\n\n')
+        
+
+            log.write(text + "\n")
+            log.write("--------------------\n")
+
+    def archive(self):
+        datepostfix = datetime.date.today().strftime("%y.%W")
+
+        archiveFile = self.logfile + "." + datepostfix
+
+        subprocess.run(['mv', self.logfile, archiveFile])
+                       
+        
+
+
+if __name__ == "__main__":
+    main()
+        
