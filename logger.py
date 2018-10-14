1df13213ec21121d28a3842f4438d8e538734bfe
z <z@debian.deb.local>
2018-10-13 19:11:07 -0500

    errors in logger

--- logger.py
+++ logger.py
@@ -7,14 +7,15 @@ from datetime import date, timedelta
 from util import getAbsolutePath
 from format import formatBlogLog, formatLinkedTootLog, formatLocalTootLog, extractExternalLinks
 
+
 def main():
     logDir = getAbsolutePath(__file__, 'log/')
     logfile = "out.txt"
     logger = Logger(logDir, logfile)
 
-
     logger.archive()
     logger.logBlog('who', 'what', 'why')
+
 
 class Logger:
     blogPostsSection = '* New Posts'
@@ -22,14 +23,11 @@ class Logger:
     opinionSection = '* Short Takes'
 
     def __init__(self, logDir, logFile):
-        self.logfile = str(logDir +  logFile)
+        self.logfile = str(logDir + logFile)
 
-        
     def logBlog(self, title, author, link):
         logBlogString = formatBlogLog(title, author, link)
         self.__insertAfterString(self.blogPostsSection, logBlogString)
-    
-                      
 
     def logToot(self, text, author):
         externalLinks = extractExternalLinks(text)
@@ -40,24 +38,25 @@ class Logger:
 
     def __logLinkedToot(self, text, author):
         linkedTootString = formatLinkedTootLog(text, author)
-        self.__insertAfterString(self.readSection, logBlogString)
+        self.__insertAfterString(self.readSection, linkedTootString)
 
     def __logLocalToot(self, text, author):
         localTootString = formatLocalTootLog(text, author)
-        self.__insertAfterString(self.opinionSection, logBlogString)
+        self.__insertAfterString(self.opinionSection, localTootString)
 
     # WARNING: this function places all of self.logfile into memory,
     #          could cause problems if file gets to big, which I don't
     #          anticipate
     def __insertAfterString(self, seekString, insertString):
         with open(self.logfile, 'r') as log:
-           logText = log.read()
+            logText = log.read()
 
-        logText = logText.replace(seekString, seekString + '\n' + insertString, 1)
+        logText = logText.replace(seekString,
+                                  seekString + '\n' + insertString, 1)
 
         with open(self.logfile, 'w') as log:
             log.write(logText)
-            
+
     def archive(self):
         datepostfix = date.today().strftime("%y.%W")
 
@@ -67,8 +66,6 @@ class Logger:
 
         self.__prepNewLogfile()
 
-       
-    
     def __prepNewLogfile(self):
         startDate = date.today()
         startDateString = startDate.strftime('%m/%d/%y')
@@ -84,11 +81,6 @@ class Logger:
             log.write(self.readSection + '\n')
             log.write(self.opinionSection + '\n')
 
-    
-        
-
 
 if __name__ == "__main__":
     main()
-        
-    
